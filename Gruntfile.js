d200dceb455975c7bb850ce3ce50153a90d6faf8
clopezdepablo <clopezdepablo@gmail.com>
2017-02-05 20:32:17 +0100

    package and gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+
+
+    grunt.initConfig({
+      
+        watch: {
+          
+            sass: {
+                files: ["src/**/*.scss"],
+                tasks: ['styles']
+            }
+            
+        },
+        
+        sass: {
+          options: {
+            style: 'expanded',
+            sourceMap: true
+          },
+          dist: {
+            files: [{
+                expand: true,
+                cwd: 'src/',
+                src: ['**/*.scss'],
+                dest: 'dist/',
+                ext: '.css'
+            }]
+          }
+        },
+        
+        autoprefixer: {
+            dist: {
+                files: {
+                    'dist/index.css': 'dist/index.css'
+                }
+            }
+        }
+        
+    });
+    
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    
+    grunt.registerTask('default', ['styles', 'watch']);
+    grunt.registerTask('styles', ['sass','autoprefixer']);
+    
+
+};
